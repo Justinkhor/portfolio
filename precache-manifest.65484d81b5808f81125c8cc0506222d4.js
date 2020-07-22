5e580ce50817349dd5bfaa6208a643161871deb1
Justinkhor <khorchengee@gmail.com>
2020-07-22 14:59:36 +0800

    Updates

---
+++ precache-manifest.65484d81b5808f81125c8cc0506222d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9393aa814556fcc28a12a551b4a34d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "424dca694bbbdb2a4ce3",
+    "url": "/portfolio/static/css/main.afa22919.chunk.css"
+  },
+  {
+    "revision": "8bda4520c54fbe7b5315",
+    "url": "/portfolio/static/js/2.42103af1.chunk.js"
+  },
+  {
+    "revision": "424dca694bbbdb2a4ce3",
+    "url": "/portfolio/static/js/main.51b21c71.chunk.js"
+  },
+  {
+    "revision": "add6ff81d93680170b15",
+    "url": "/portfolio/static/js/runtime-main.2601f1e9.js"
+  }
+]);
